8283b580f3364150fec0b67b9bad53cf1610da79
Sreelakshmi T U <sreelakshmiudhayan@gmail.com>
2023-08-08 14:04:29 +0530

    CRUD actions updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
     root "pages#home"
 get "about",to:"pages#about"
-resources :articles, only: [:show]
-resources :articles, only: [:show, :index]
+resources :articles
 
 end
 
